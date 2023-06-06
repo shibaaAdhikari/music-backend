4ca827e2d7bedfaa85af88d35a69df4cbc35b2b9
Shibaa <shibaaadhikari0@gmail.com>
2023-06-06 14:28:19 +0545

    added plugin file

---
+++ controller/authController.js
@@ -0,0 +1,28 @@
+export const signUpHandler = async (req, reply) => {
+  try {
+    const user = await req.server.user.create({
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password,
+    });
+    reply.send(user);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const signInHandler = async (req, reply) => {
+  try {
+    const user = await req.server.user.findOne({
+      where: { username: req.body.username },
+    });
+
+    if (user.password === req.body.password) {
+      reply.code(200).send({ msg: "Logged in successfully" });
+    } else {
+      reply.code(401).send({ msg: "Incorrect password" });
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
