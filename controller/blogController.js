7da4d5f4187d03a470f8a07b8ef515249031296f
Shibaa <shibaaadhikari0@gmail.com>
2023-06-07 14:57:28 +0545

    added deleted functions

--- controller/blogController.js
+++ controller/blogController.js
@@ -1,13 +1,38 @@
 export const getsBlogsHandler = async (req, reply) => {
   try {
-    const blogs = await req.server.blog.findAll();
+    const blogs = await req.server.users.findAll();
     await reply.code(200).view("/blogs.ejs", { blogs });
   } catch (err) {
     console.log(err);
   }
 };
 
-export const getUserById = async (req,reply)=>{
+export const postBlogsHandler = async (req, reply) => {
+  try {
+    const blog = await req.server.blog.create({
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password,
+    });
+    reply.redirect("/register");
+  } catch (err) {
+    console.log(err);
+  }
+};
 
-  
-}
+export const deleteBlogsHandler = async (req, reply) => {
+  try {
+    const userId = req.params.userId;
+    const deletedUser = await req.server.users.destroy({ where: { userId } });
+    if (deletedUser) {
+      reply.code(200).send({ message: "User deleted successfully" });
+    } else {
+      reply.code(404).send({ message: "User not found" });
+    }
+  } catch (error) {
+    reply.code(500).send({ message: "Internal server error" });
+  }
+};
+
+
+
--- routes/blogs/index.js
+++ routes/blogs/index.js
@@ -1,10 +1,14 @@
-import { getsBlogsHandler } from "../../controller/blogController.js";
+import {
+  deleteBlogsHandler,
+  getsBlogsHandler,
+} from "../../controller/blogController.js";
 
 export default async function (fastify, opts) {
   const getBlogsOptns = {
     schema: {},
     handler: getsBlogsHandler,
   };
+
   fastify.get("/blogs", async function (request, reply) {
     const users = await request.server.users.findAll();
     const mappedUsers = users.map((user) => user.dataValues);
@@ -12,7 +16,11 @@ export default async function (fastify, opts) {
     await reply.view("/blogs.ejs", { mappedUsers: mappedUsers });
   });
 
-  
+  const deleteBlogsOptns = {
+    schema: {},
+    handler: deleteBlogsHandler,
+  };
 
   fastify.post("/blogs", getBlogsOptns);
+  fastify.delete("/blogs/:userId", deleteBlogsOptns);
 }
