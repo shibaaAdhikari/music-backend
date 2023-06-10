ebd98aa75dc8238142f83a10bef23a177c76c3e4
Shibaa <shibaaadhikari0@gmail.com>
2023-06-10 20:27:13 +0545

    fixes mailer issue

--- controller/authController.js
+++ controller/authController.js
@@ -1,7 +1,44 @@
 const signUpHandler = async (req, reply) => {
   try {
-    const user = await req.server.users.create(req.body);
-    reply.redirect("/user/login");
+    const user = await req.server.users.create({
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password,
+    });
+
+    req.server.transporter.sendMail({
+      from: "shibaaadhikari0@gmail.com",
+      to: user.email,
+      subject: "Registration Successful",
+      html: `<html>
+      <body>
+      <p>Hi ${user.username},</p>
+        <h2>Sign In Successful!</h2>
+        <p> Welcome to my blog.Thanks for signing up.</p>
+      </body>
+      
+      <\html>`,
+    });
+    // const { users } = req.server;
+    // const newUser = await users.create(req.body);
+
+    // req.server.transporter.sendMail({
+    //   from:"shibaaadhikari0@gmail.com",
+    //   to:user
+    // })
+
+    // const emailOptions = {
+    //   from: "shibaaadhikari0@gmail.com",
+    //   to: user.email,
+    //   subject: "Registration Successful",
+    //   text: "Thank you for registering on our website",
+    // };
+
+    // await req.server.sendEmail(emailOptions);
+
+    await reply.view("/user/login", {
+      message: "User created successfully.Please login to continue",
+    });
   } catch (err) {
     console.log(err);
   }
