eb393f8a3f584f718207fe4eaf5a2314f678868a
Shibaa <shibaaadhikari0@gmail.com>
2023-06-12 12:10:09 +0545

    added header,footer.ejs

--- controller/authController.js
+++ controller/authController.js
@@ -1,3 +1,8 @@
+import { renderFile } from "ejs";
+import path from "path";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
 const signUpHandler = async (req, reply) => {
   try {
     const user = await req.server.users.create({
@@ -6,41 +11,33 @@ const signUpHandler = async (req, reply) => {
       password: req.body.password,
     });
 
-    req.server.transporter.sendMail({
-      from: "shibaaadhikari0@gmail.com",
-      to: user.email,
-      subject: "Registration Successful",
-      html: `<html>
-      <body>
-      <p>Hi ${user.username},</p>
-        <h2>Sign In Successful!</h2>
-        <p> Welcome to my blog.Thanks for signing up.</p>
-      </body>
-      
-      <\html>`,
-    });
-    // const { users } = req.server;
-    // const newUser = await users.create(req.body);
+    const moduleURL = import.meta.url;
+    const modulePath = dirname(fileURLToPath(moduleURL));
+    const templatePath = path.join(
+      modulePath,
+      "..",
+      "template",
+      "emailTemplate.ejs"
+    );
 
-    // req.server.transporter.sendMail({
-    //   from:"shibaaadhikari0@gmail.com",
-    //   to:user
-    // })
+    const emailTemplate = await renderFile(templatePath, user.username);
 
-    // const emailOptions = {
-    //   from: "shibaaadhikari0@gmail.com",
-    //   to: user.email,
-    //   subject: "Registration Successful",
-    //   text: "Thank you for registering on our website",
-    // };
-
-    // await req.server.sendEmail(emailOptions);
+    try {
+      await req.server.transporter.sendMail({
+        from: "shibaaadhikari0@gmail.com",
+        to: user.email,
+        subject: "Registration Successful",
+        html: emailTemplate,
+      });
+    } catch (err) {
+      console.error("Error sending email:", err);
+    }
 
     await reply.redirect("/user/login", {
-      message: "User created successfully.Please login to continue",
+      message: "User created successfully. Please login to continue.",
     });
   } catch (err) {
-    console.log(err);
+    console.error(err);
   }
 };
 
@@ -52,6 +49,7 @@ const signInHandler = async (req, reply) => {
     const user = await users.findOne({
       where: { email },
     });
+
     if (!user) {
       return reply.code(401).send({ msg: "User not found" });
     }
@@ -62,7 +60,7 @@ const signInHandler = async (req, reply) => {
       reply.code(401).send({ msg: "Incorrect password" });
     }
   } catch (err) {
-    console.log(err);
+    console.error(err);
   }
 };
 
